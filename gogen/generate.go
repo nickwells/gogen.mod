6f6bacab0ed3e0cacd211f21f2066bc10b659786
Nick Wells <wells.nicholas@gmail.com>
2021-02-15 19:40:57 +0000

    add a build tag to the generate.go files

--- gogen/generate.go
+++ gogen/generate.go
@@ -1,3 +1,5 @@
+// +build generate
+
 package gogen
 
 //go:generate mkfunccontrolparamtype -d "This determines whether or not a go subcommand should be run with its output displayed" -t CmdIO -v NoCmdIO -v ShowCmdIO
