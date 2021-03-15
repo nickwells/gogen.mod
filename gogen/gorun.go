86e73efb6803008646ef0294e8031730cbec59a9
Nick Wells <wells.nicholas@gmail.com>
2021-03-15 12:19:19 +0000

    Add the ExecGoCmdCaptureOutput func

--- gogen/gorun.go
+++ gogen/gorun.go
@@ -2,10 +2,23 @@ package gogen
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+// runGoOrDie runs the command and exits if it fails
+func runGoOrDie(cmd *exec.Cmd) {
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't exec the command")
+		fmt.Fprintf(os.Stderr, "\t%s %s\n",
+			cmd.Path, strings.Join(cmd.Args, " "))
+		fmt.Fprintln(os.Stderr, "\tError:", err)
+		os.Exit(1)
+	}
+}
 
 // ExecGoCmd will exec the go program with the supplied arguments. If it
 // detects an error it will report it and exit
@@ -16,11 +29,16 @@ func ExecGoCmd(ioMode CmdIOType, args ...string) {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 	}
-	err := cmd.Run()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Couldn't exec the go command")
-		fmt.Fprintln(os.Stderr, "\tgo "+strings.Join(args, " "))
-		fmt.Fprintln(os.Stderr, "\tError:", err)
-		os.Exit(1)
+	runGoOrDie(cmd)
+}
+
+// ExecGoCmdCaptureOutput will exec the go program with the supplied
+// arguments. The command's Stdout is connected to the supplied
+// writer if it is not nil.
+func ExecGoCmdCaptureOutput(w io.Writer, args ...string) {
+	cmd := exec.Command("go", args...)
+	if w != nil {
+		cmd.Stdout = w
 	}
+	runGoOrDie(cmd)
 }
