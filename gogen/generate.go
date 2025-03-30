e41703d171ee5402a11bb3e57c712fae2d10119b
Nick Wells <wells.nicholas@gmail.com>
2025-03-31 00:46:06 +0100

    refactor: regenerate the typeCmdIO.go file
    This is generated with the new version of the mkfunccontrolparamtype command
    which wraps the text commenting the type name and adds a comment before the
    const values.

--- gogen/generate.go
+++ gogen/generate.go
@@ -2,4 +2,4 @@
 
 package gogen
 
-//go:generate mkfunccontrolparamtype -d "This determines whether or not a go subcommand should be run with its output displayed. You can also choose to not show IO on failure" -t CmdIO -v NoCmdIO -v ShowCmdIO -v NoCmdFailIO
+//go:generate mkfunccontrolparamtype -d "determines whether or not a go subcommand should be run with its output displayed. You can also choose to not show IO on failure" -t CmdIO -v NoCmdIO -v ShowCmdIO -v NoCmdFailIO
